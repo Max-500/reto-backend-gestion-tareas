844f858a7252c74d437f81ea413441e87bb6c48c
Max-500 <213497@ids.upchiapas.edu.mx>
2023-06-17 14:52:28 -0600

    Error de Ortografia solucionado

--- src/configurations/rateLimiter.js
+++ src/configurations/rateLimiter.js
@@ -4,7 +4,7 @@ import rateLimiter from "express-rate-limit";
 // Middleware de limitación de velocidad utilizando express-rate-limit
 const limiter = rateLimiter({
     windowMs: 5 * 60 * 1000, // Tiempo en que se genera una nueva sesion (5 minutos en este ejemplo)
-    max: 30, // Número máximo de peticiones permitidas durante la sesion
+    max: 50, // Número máximo de peticiones permitidas durante la sesion
     handler: (req, res, next) => { // Manejador personalizado para responder a solicitudes que exceden el límite
       res.status(429).json({ error: 'Demasiadas solicitudes. Por favor, intenta de nuevo más tarde.' });
     },
